48dc0d6fc933854dd001799ea321b3f043041597
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-06-07 14:36:27 +0300

    finished the components and the app file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Map } from "./Components/Map";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Map></Map>
+    </div>
+  );
 }
 
 export default App;
